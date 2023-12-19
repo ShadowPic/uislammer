0220926134b66bf0f0f3481539b0cb113ec24a6e
Rahul Dhepe <radhepe@microsoft.com>
2023-12-19 18:42:44 +0530

    Add project files.

---
+++ Setup/Config.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Playwright;
+
+namespace playwrightBDD.Setup
+{
+    public class Config
+    {
+        public static bool HEADLESS = Convert.ToBoolean(CommonUtil.GetAValueFromAppSettings("Playwright:LaunchOptions:Headless"));
+        public static bool ENVIRONMENT = Convert.ToBoolean(CommonUtil.GetAValueFromAppSettings("Playwright:LaunchOptions:Headless"));
+    }
+}
---
+++ Setup/Hooks.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+namespace playwrightBDD.Setup
+{
+    [Binding]
+    public sealed class Hooks :BaseStepDefinition
+    {
+        public IPage currentPage { get { return base.PlaywrightPage; } }
+        [BeforeScenario]
+        public async Task RegisterSingleInstancePractitioner()
+        {
+            base.CleanupPlaywright();
+            await base.InitializePlaywright();
+        }
+    }
+}
